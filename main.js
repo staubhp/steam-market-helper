a53d7abbb1381732d85f827ad7ec1261dd641402
Payton Staub <staubhpa@gmail.com>
2015-07-20 14:48:07 -0700

    Now parsing listings

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var mySearcher = new Searcher();
 $( document ).ready(function(){
 mySearcher.search();
+//mySearcher.getProduct('http://steamcommunity.com/market/listings/570/Auspicious%20Bracers%20of%20the%20Clergy%20Ascetic');
 });
---
+++ product.js
@@ -0,0 +1,16 @@
+Product = function(){
+	this.listings = [];
+	this.avgPrice = -1;
+	this.minPrice = -1;
+	this.diffPrice = -1;
+	this.url = "";
+	this.diffPriceWithoutFee = -1;
+	this.averages = [];
+	
+}
+
+Product.prototype.buy = function(){
+}
+
+Product.prototype.sell = function(){
+}
